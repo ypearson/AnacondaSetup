54cc2f57a8d93e1dbfa79ca52313c87bc20dcab8
Yvan Pearson <ypearson@nklabs.com>
2018-02-21 10:58:54 -0500

    ensure python2 gets install if selected.

--- getTools.sh
+++ getTools.sh
@@ -14,6 +14,7 @@ ANA_VER=5.1.0
 ANACONDA=anaconda
 INSTALLER=Anaconda${1}-${ANA_VER}-Linux-x86_64.sh
 ANACONDA_LINK=http://repo.continuum.io/archive/${INSTALLER}
+ENV_FILE=environment.yml
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -21,10 +22,14 @@ sudo apt-get install -y qt5-default libqt5opengl5-dev libgtk-3-dev libgtkglext1 
 libgtkglext1-dev libpng3 pngtools libpng12-dev libpng12-0 libpng++-dev libtiff5-dev \
 libtiff5 libtiff-tools libjpeg8-dev libjpeg8 libjpeg8-dbg libjasper-dev libjasper-runtime
 
+if [[ $1 -eq 2 ]]; then
+    sed -i -e 's/3.6/2.7/g' $ENV_FILE
+fi
+
 if [ ! -d $HOME/${ANACONDA} ]; then
     wget ${ANACONDA_LINK} && chmod +x ${INSTALLER} && ./${INSTALLER} -b -p $HOME/$ANACONDA
 fi
 
-$HOME/${ANACONDA}/bin/conda env create -f environment.yml
+$HOME/${ANACONDA}/bin/conda env create -f $ENV_FILE
 
 echo ". ~/anaconda/bin/activate opencv_env"
