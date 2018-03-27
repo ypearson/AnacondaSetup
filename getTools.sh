80df2d9af2cc66714a18d630bbae3a240b36fff4
Yvan Pearson <ypearson@nklabs.com>
2018-03-27 16:32:26 -0400

    removed python 2 option

--- getTools.sh
+++ getTools.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Input python version, v2 or v3"
-  exit 1
-fi
-
-if [[ $1 -ne 2 && $1 -ne 3 ]]; then
-    echo "Choose 2 or 3"
-    exit 1
-fi
-
 ANA_VER=5.1.0
 ANACONDA=anaconda
-INSTALLER=Anaconda${1}-${ANA_VER}-Linux-x86_64.sh
+INSTALLER=Anaconda3-${ANA_VER}-Linux-x86_64.sh
 ANACONDA_LINK=http://repo.continuum.io/archive/${INSTALLER}
 ENV_FILE=environment.yml
 
@@ -22,14 +12,20 @@ sudo apt-get install -y qt5-default libqt5opengl5-dev libgtk-3-dev libgtkglext1 
 libgtkglext1-dev libpng3 pngtools libpng12-dev libpng12-0 libpng++-dev libtiff5-dev \
 libtiff5 libtiff-tools libjpeg8-dev libjpeg8 libjpeg8-dbg libjasper-dev libjasper-runtime
 
-if [[ $1 -eq 2 ]]; then
-    sed -i -e 's/3.6/2.7/g' $ENV_FILE
-fi
-
 if [ ! -d $HOME/${ANACONDA} ]; then
-    wget ${ANACONDA_LINK} && chmod +x ${INSTALLER} && ./${INSTALLER} -b -p $HOME/$ANACONDA
+
+    if [ ! -f ${INSTALLER} ]; then
+
+        if ! wget ${ANACONDA_LINK}; then
+            exit 1
+        fi
+    fi
+
+    chmod +x ${INSTALLER} && ./${INSTALLER} -b -p $HOME/$ANACONDA
 fi
 
 $HOME/${ANACONDA}/bin/conda env create -f $ENV_FILE
 
-echo ". ~/anaconda/bin/activate opencv_env"
+source ~/anaconda/bin/activate opencv_env
+python ./test.py
+
